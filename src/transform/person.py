25869a9f8b2296e04b0dcd082b873014d361613f
Effrat <effratk@gmail.com>
2022-03-15 19:36:09 +0200

    person cleanup

--- src/transform/person.py
+++ src/transform/person.py
@@ -1,4 +1,4 @@
-# TODO: add urls for images and mk pages
+# TODO: add urls for images and mk pages?
 import pandas as pd
 
 
@@ -8,21 +8,17 @@ def person():
     '''
     KNS_Person = pd.read_excel('../data/raw/KNS_Person.xlsx', index_col=0)
     KNS_Person = KNS_Person[['PersonID', 'LastName', 'FirstName', 'GenderDesc']].drop_duplicates()
-    KNS_Person
+
     KNS_MkSiteCode = pd.read_excel('../data/raw/KNS_MkSiteCode.xlsx', index_col=0)
     KNS_MkSiteCode = KNS_MkSiteCode[['KnsID', 'SiteId']]
     KNS_MkSiteCode.rename(columns={'KnsID': 'PersonID'}, inplace=True)
-    KNS_MkSiteCode
 
-    person = pd.merge(
-        KNS_Person, KNS_MkSiteCode,
+    person = pd.merge(KNS_Person, KNS_MkSiteCode,
         on='PersonID', how='left')
     person['SiteId'] = person['SiteId'].astype('Int64')
     person['FullName'] = person['FirstName'] + ' ' + person['LastName']
     person.columns = ['person_id', 'last_name', 'first_name', 'gender', 'site_id', 'full_name']
+
     person.to_excel(
         '../data/model/dimensions/person.xlsx',
-        sheet_name='person',
-        index=False
-        )
-    person
+        sheet_name='person', index=False)
