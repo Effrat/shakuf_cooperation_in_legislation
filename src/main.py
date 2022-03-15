038e4c493842916f22a75129fcbc0d045bce372e
Effrat <effratk@gmail.com>
2022-03-15 21:23:54 +0200

    all_bills_sponsors cleanup

--- src/main.py
+++ src/main.py
@@ -7,8 +7,8 @@ from transform.knesset import knesset
 from transform.knesset_by_date import knesset_by_date
 from transform.person import person
 from transform.bill_to_date_from_session import bill_to_date_from_session
-from transform.bill_to_date_from_commettee_session import bill_to_date_from_commettee_session
-from transform.all_bill_initiators import all_bill_initiators
+from transform.bill_to_date_from_committee_session import bill_to_date_from_committee_session
+from transform.all_bill_sponsors import all_bill_sponsors
 from transform.members_of_knesset_by_date import members_of_knesset_by_date
 from testing.members_of_knesset_by_date_test import members_of_knesset_by_date_test
 from transform.people_in_government_by_date import people_in_government_by_date
@@ -17,9 +17,9 @@ from transform.dates import dates
 from transform.bill_to_date import bill_to_date
 from transform.members_of_knesset_faction_by_date import members_of_knesset_faction_by_date
 from transform.faction_side_by_date import faction_side_by_date
-from transform.all_bill_initiators_to_date import all_bill_initiators_to_date
+from transform.all_bill_sponsors_to_date import all_bill_sponsors_to_date
 from transform.members_of_knesset_faction_and_side_by_date import members_of_knesset_faction_and_side_by_date
-from transform.bill_initiators import bill_initiators
+from transform.bill_sponsors import bill_sponsors
 from transform.bill_to_side import bill_to_side
 from transform.bill import bill
 
@@ -39,7 +39,7 @@ def update_data():
     # person()
     # bill_to_date_from_session()
     # bill_to_date_from_commettee_session()
-    # all_bill_initiators()
+    # all_bill_sponsors()
     # members_of_knesset_by_date()
     # members_of_faction_by_date()
     # people_in_government_by_date()
@@ -52,18 +52,18 @@ def update_data():
 
     # # ----- step 3 -----
     # faction_side_by_date_test #TODO: create unit test
-    # all_bill_initiators_to_date()
+    # all_bill_sponsors_to_date()
     # members_of_knesset_faction_and_side_by_date()
 
     # # ----- step 4 -----
-    # test_all_bill_initiators_to_date #TODO: create unit test
+    # test_all_bill_sponsors_to_date #TODO: create unit test
     # members_of_knesset_faction_and_side_by_date_test() #TODO: create unit test
 
     # # ----- step 5 -----
-    bill_initiators()
+    bill_sponsors()
 
     # # ----- step 6 -----
-    # test_bill_initiators() #TODO: create unit test
+    # test_bill_sponsors() #TODO: create unit test
 
     # # ----- step 7 -----
     bill_to_side()
--- src/transform/all_bill_sponsors.py
+++ src/transform/all_bill_sponsors.py
@@ -1,22 +1,10 @@
 import pandas as pd
 
 
-def all_bill_initiators():
+def all_bill_sponsors():
     """
     Creates all_bill_initiators table from KNS_BillInitiator and KNS_BillHistoryInitiator.
     """
-# members_of_knesset_faction_and_side_by_date = pd.read_csv(
-#     '../data/transformed/members_of_knesset_faction_and_side_by_date.csv')
-# members_of_knesset_faction_and_side_by_date['date'] = pd.to_datetime(
-#     members_of_knesset_faction_and_side_by_date['date'])
-# print(members_of_knesset_faction_and_side_by_date.info())
-# members_of_knesset_faction_and_side_by_date.info()
-# bill_to_date = pd.read_excel(
-#     '../data/transformed/bill_to_date.xlsx',
-#     parse_dates=['date'])
-# bill_to_date
-# print(bill_to_date.info())
-
     KNS_BillInitiator = pd.read_excel(
         '../data/raw/KNS_BillInitiator.xlsx',
         index_col=0)
@@ -24,32 +12,22 @@ def all_bill_initiators():
     KNS_BillInitiator.columns = ['bill_id', 'person_id', 'is_initiator']
     KNS_BillInitiator['is_initiator'].fillna(False, inplace=True)
     KNS_BillInitiator['is_initiator'] = KNS_BillInitiator['is_initiator'].astype(bool)
-    KNS_BillInitiator
-    print(KNS_BillInitiator.info())
-    KNS_BillInitiator
 
     KNS_BillHistoryInitiator = pd.read_excel(
         '../data/raw/KNS_BillHistoryInitiator.xlsx',
         index_col=0)
     KNS_BillHistoryInitiator.drop(columns=[
-        'BillHistoryInitiatorID',
-        'StartDate',
-        'EndDate',
-        'ReasonID',
-        'ReasonDesc',
-        'LastUpdatedDate'], inplace=True)
+        'BillHistoryInitiatorID', 'StartDate', 'EndDate',
+        'ReasonID', 'ReasonDesc', 'LastUpdatedDate'], inplace=True)
     KNS_BillHistoryInitiator.columns = ['bill_id', 'person_id', 'is_initiator']
     KNS_BillHistoryInitiator['is_initiator'].fillna(False, inplace=True)
     KNS_BillHistoryInitiator['is_initiator'] = KNS_BillHistoryInitiator['is_initiator'].astype(bool)
-    KNS_BillHistoryInitiator
-    print(KNS_BillHistoryInitiator.info())
-    KNS_BillHistoryInitiator
+
 
     all_bill_sponsors = pd.concat([KNS_BillInitiator, KNS_BillHistoryInitiator])
     all_bill_sponsors = all_bill_sponsors.groupby(['bill_id', 'person_id']).agg('max')
     all_bill_sponsors.reset_index(inplace=True)
+
     all_bill_sponsors.to_excel(
         '../data/transformed/all_bill_sponsors.xlsx',
-        sheet_name='all_bill_sponsors',
-        index=False)
-    all_bill_sponsors
+        sheet_name='all_bill_sponsors', index=False)
