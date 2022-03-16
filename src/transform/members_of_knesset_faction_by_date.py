574a47befcac5704fc5938961f87d263ab007db7
Effrat <effratk@gmail.com>
2022-03-16 13:26:59 +0200

    members_of_knesset_faction_by_date cleanup

--- src/transform/members_of_knesset_faction_by_date.py
+++ src/transform/members_of_knesset_faction_by_date.py
@@ -5,27 +5,22 @@ def members_of_knesset_faction_by_date():
     """
     Creates members_of_knesset_faction_by_date table from members_of_knesset_by_date and members_of_faction_by_date.
     """
-
     members_of_knesset_by_date = pd.read_csv(
         '../data/transformed/members_of_knesset_by_date.csv',
         parse_dates=['date'])
-    members_of_knesset_by_date
 
     members_of_faction_by_date = pd.read_csv(
         '../data/transformed/members_of_faction_by_date.csv',
         parse_dates=['date'])
-    members_of_faction_by_date
 
 
-    # ---- members of Knesset who are also members of a faction ----
     members_of_knesset_faction_by_date = pd.merge(
-        members_of_knesset_by_date, members_of_faction_by_date, how='left')
+        members_of_knesset_by_date, members_of_faction_by_date, how='outer')
+        # if knesset_num is NaN, then not a Member of Knesset (Norwegian?)
     members_of_knesset_faction_by_date.sort_values('date', inplace=True)
     members_of_knesset_faction_by_date.drop_duplicates(
         subset=['date', 'person_id'], inplace=True, keep='first')
 
 
     members_of_knesset_faction_by_date.to_csv(
-        '../data/transformed/members_of_knesset_faction_by_date.csv',
-        index=False)
-    members_of_knesset_faction_by_date
+        '../data/transformed/members_of_knesset_faction_by_date.csv', index=False)
