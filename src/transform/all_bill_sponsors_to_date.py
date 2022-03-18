470429049002b3f9c480ec04495a168c6cc8eb73
Effrat <effratk@gmail.com>
2022-03-18 13:27:33 +0200

    replaced initiators w/ sponsors

--- src/transform/all_bill_sponsors_to_date.py
+++ src/transform/all_bill_sponsors_to_date.py
@@ -2,17 +2,17 @@ import pandas as pd
 
 def all_bill_sponsors_to_date():
     """
-    Creates all_bill_sponsors_to_date table from bill_to_date and all_bills_initiators.
+    Creates all_bill_sponsors_to_date table from bill_to_date and all_bills_sponsors.
     """
     bill_to_date = pd.read_excel(
         '../data/transformed/bill_to_date.xlsx',
         parse_dates=['date'])
 
-    all_bills_initiators = pd.read_excel(
-        '../data/transformed/all_bills_initiators.xlsx')
+    all_bills_sponsors = pd.read_excel(
+        '../data/transformed/all_bills_sponsors.xlsx')
 
     all_bill_sponsors_to_date = pd.merge(
-        bill_to_date, all_bills_initiators, on='bill_id', how='right')
+        bill_to_date, all_bills_sponsors, on='bill_id', how='right')
 
     all_bill_sponsors_to_date.to_excel(
         '../data/transformed/all_bill_sponsors_to_date.xlsx',
