a9e8f91d03a214e1c4b005212d9a7e9125b86803
Effrat <effratk@gmail.com>
2022-03-18 15:31:00 +0200

    added automatic errors report output

--- src/transform/bill_to_date_from_committee_session.py
+++ src/transform/bill_to_date_from_committee_session.py
@@ -4,14 +4,7 @@ def bill_to_date_from_committee_session():
     """
     Creates bill_to_date_from_committee_session table from KNS_CmtSessionItem and KNS_CommitteeSession.
     """
-    KNS_CmtSessionItem = pd.read_excel(
-        '../data/raw/KNS_CmtSessionItem.xlsx', index_col=0)
-
-    # ---- filter only bills
-    bills_committee_sessions = KNS_CmtSessionItem[KNS_CmtSessionItem['ItemTypeID'] == 2][['ItemID', 'CommitteeSessionID']]
-    bills_committee_sessions.columns = ['bill_id', 'session_id']
-    bills_committee_sessions = bills_committee_sessions.groupby('bill_id').agg('min').reset_index()
-
+    # ----- load -----
     KNS_CommitteeSession = pd.read_excel(
         '../data/raw/KNS_CommitteeSession.xlsx',
         parse_dates=['StartDate'], index_col=0)
@@ -19,12 +12,33 @@ def bill_to_date_from_committee_session():
     committee_session_to_date = KNS_CommitteeSession[['CommitteeSessionID', 'StartDate']]
     committee_session_to_date.columns = ['session_id', 'date']
 
+    KNS_CmtSessionItem = pd.read_excel(
+        '../data/raw/KNS_CmtSessionItem.xlsx', index_col=0)  
+    bills_committee_sessions = KNS_CmtSessionItem[KNS_CmtSessionItem['ItemTypeID'] == 2] # filter only bills
+    bills_committee_sessions = bills_committee_sessions[['ItemID', 'CommitteeSessionID']]
+    bills_committee_sessions.columns = ['bill_id', 'session_id']
+    bills_committee_sessions = bills_committee_sessions.groupby('bill_id').agg('min').reset_index()
+
+    # ----- join -----
     bill_to_date_from_committee_session = pd.merge(
-        bills_committee_sessions, committee_session_to_date,
+        committee_session_to_date, bills_committee_sessions,
         on='session_id', how='left')
     bill_to_date_from_committee_session = bill_to_date_from_committee_session.drop(columns=['session_id'])
     bill_to_date_from_committee_session['date'] = bill_to_date_from_committee_session['date'].dt.date
 
+    # ----- testing/feedback -----
+    nan_dates_size = bill_to_date_from_committee_session[bill_to_date_from_committee_session['date'].isnull()].size
+    if nan_dates_size == 0:
+        feedback = 'There are no relevant sessions w/o date data.'
+    else:
+        errors = bill_to_date_from_committee_session[bill_to_date_from_committee_session['date'].isnull()]
+        errors = str(errors.to_dict())
+        feedback = 'Relevant sessions w/o date data:' + '\n' + errors
+    f = open('../data/reports/bill_to_date_from_committee_session_errors.txt','w+')
+    f.write(feedback)
+    f.close()
+
+    # ----- save -----
     bill_to_date_from_committee_session.to_excel(
         '../data/transformed/bill_to_date_from_committee_session.xlsx',
         sheet_name='bill_to_date_from_committee_session',
--- src/transform/bill_to_date_from_session.py
+++ src/transform/bill_to_date_from_session.py
@@ -4,27 +4,43 @@ def bill_to_date_from_session():
     """
     Creates bill_to_date_from_session table from KNS_PlenumSession and KNS_PlmSessionItem.
     """
-    KNS_PlmSessionItem = pd.read_excel(
-        '../data/raw/KNS_PlmSessionItem.xlsx',
-        index_col=0)
-
-    # ---- filter only bills
-    bills_sessions = KNS_PlmSessionItem[KNS_PlmSessionItem['ItemTypeID'] == 2][['ItemID', 'PlenumSessionID']]
-    bills_sessions.columns = ['bill_id', 'session_id']
-    bills_sessions = bills_sessions.groupby('bill_id').agg('min').reset_index()
-
+    # ----- load -----
     KNS_PlenumSession = pd.read_excel(
         '../data/raw/KNS_PlenumSession.xlsx',
         parse_dates=['StartDate'], index_col=0)
     session_to_date = KNS_PlenumSession[['PlenumSessionID', 'StartDate']]
     session_to_date.columns = ['session_id', 'date']
     session_to_date['date'] = session_to_date['date'].dt.date
+    session_to_date
 
+    KNS_PlmSessionItem = pd.read_excel(
+        '../data/raw/KNS_PlmSessionItem.xlsx', index_col=0)
+    bills_sessions = KNS_PlmSessionItem[KNS_PlmSessionItem['ItemTypeID'] == 2] # filter only bills
+    bills_sessions = bills_sessions[['ItemID', 'PlenumSessionID']]
+    bills_sessions.columns = ['bill_id', 'session_id']
+    bills_sessions = bills_sessions.groupby('bill_id').agg('min').reset_index()
+    bills_sessions
+
+    # ----- join -----
     bill_to_date_from_session = pd.merge(
-        bills_sessions, session_to_date,
-        on='session_id', how='left')
+        session_to_date, bills_sessions,
+        on='session_id', how='right')
     bill_to_date_from_session = bill_to_date_from_session.drop(columns=['session_id'])
+    bill_to_date_from_session
 
+    # ----- testing/feedback -----
+    nan_dates_size = bill_to_date_from_session[bill_to_date_from_session['date'].isnull()].size
+    if nan_dates_size == 0:
+        feedback = 'There are no relevant sessions w/o date data.'
+    else:
+        errors = bill_to_date_from_session[bill_to_date_from_session['date'].isnull()]
+        errors = str(errors.to_dict())
+        feedback = 'Relevant sessions w/o date data:' + '\n' + errors
+    f = open('../data/reports/bill_to_date_from_session_errors.txt','w+')
+    f.write(feedback)
+    f.close()
+
+    # ----- save -----
     bill_to_date_from_session.to_excel(
         '../data/transformed/bill_to_date_from_session.xlsx',
         sheet_name='bill_to_date_from_session', index=False)
