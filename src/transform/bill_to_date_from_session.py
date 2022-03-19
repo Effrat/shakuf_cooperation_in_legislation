3b462da8ddd77113855963b11ec518dd0af99572
Effrat <effratk@gmail.com>
2022-03-19 15:23:47 +0200

    output errors to csv

--- src/transform/bill_to_date_from_session.py
+++ src/transform/bill_to_date_from_session.py
@@ -3,7 +3,9 @@ import pandas as pd
 def bill_to_date_from_session():
     """
     Creates bill_to_date_from_session table from KNS_PlenumSession and KNS_PlmSessionItem.
+    Outputs report: '../data/reports/nan_dates_from_session.csv'
     """
+
     # ----- load -----
     KNS_PlenumSession = pd.read_excel(
         '../data/raw/KNS_PlenumSession.xlsx',
@@ -21,24 +23,19 @@ def bill_to_date_from_session():
     bills_sessions = bills_sessions.groupby('bill_id').agg('min').reset_index()
     bills_sessions
 
-    # ----- join -----
+
+    # ----- transform -----
     bill_to_date_from_session = pd.merge(
         session_to_date, bills_sessions,
         on='session_id', how='right')
     bill_to_date_from_session = bill_to_date_from_session.drop(columns=['session_id'])
     bill_to_date_from_session
 
+
     # ----- testing/feedback -----
-    nan_dates_size = bill_to_date_from_session[bill_to_date_from_session['date'].isnull()].size
-    if nan_dates_size == 0:
-        feedback = 'There are no relevant sessions w/o date data.'
-    else:
-        errors = bill_to_date_from_session[bill_to_date_from_session['date'].isnull()]
-        errors = str(errors.to_dict())
-        feedback = 'Relevant sessions w/o date data:' + '\n' + errors
-    f = open('../data/reports/bill_to_date_from_session_errors.txt','w+')
-    f.write(feedback)
-    f.close()
+    nan_dates_from_session = bill_to_date_from_session[bill_to_date_from_session['date'].isnull()]
+    nan_dates_from_session.to_csv('../data/reports/nan_dates_from_session.csv', index=False)
+
 
     # ----- save -----
     bill_to_date_from_session.to_excel(
