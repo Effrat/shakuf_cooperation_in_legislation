b6585e9312f6971055f28ce148c67bed235dabc1
Effrat <effratk@gmail.com>
2022-03-20 15:01:20 +0200

    aesthetics

--- src/transform/bill_to_date.py
+++ src/transform/bill_to_date.py
@@ -4,6 +4,7 @@ def bill_to_date():
     """
     Creates bill_to_date table from bill_to_date_from_commettee_session and bill_to_date_from_session.
     """
+    # ----- load -----
     bill_to_date_from_commettee_session = pd.read_excel(
         '../data/transformed/bill_to_date_from_committee_session.xlsx',
         parse_dates=['date'])
@@ -12,6 +13,7 @@ def bill_to_date():
         '../data/transformed/bill_to_date_from_session.xlsx',
         parse_dates=['date'])
 
+    # ----- transform -----
     bill_to_date = pd.concat([
         bill_to_date_from_commettee_session,
         bill_to_date_from_session])
@@ -19,6 +21,7 @@ def bill_to_date():
     bill_to_date = bill_to_date.groupby('bill_id').agg('min').reset_index()
     bill_to_date['date'] = bill_to_date['date'].dt.date
 
+    # ----- save -----
     bill_to_date.to_excel(
         '../data/transformed/bill_to_date.xlsx',
         sheet_name='bill_to_date', index=False)
