4f6c61735005f1118651f033bbb8358e53914d7a
Effrat <effratk@gmail.com>
2022-03-22 18:47:11 +0200

    to function format

--- src/transform/person.py
+++ src/transform/person.py
@@ -8,10 +8,10 @@ def person():
     '''
 
     # ----- load -----
-    KNS_Person = pd.read_excel('../../data/raw/KNS_Person.xlsx', index_col=0)
+    KNS_Person = pd.read_excel('../data/raw/KNS_Person.xlsx', index_col=0)
     KNS_Person = KNS_Person[['PersonID', 'LastName', 'FirstName', 'GenderDesc']].drop_duplicates()
 
-    KNS_MkSiteCode = pd.read_excel('../../data/raw/KNS_MkSiteCode.xlsx', index_col=0)
+    KNS_MkSiteCode = pd.read_excel('../data/raw/KNS_MkSiteCode.xlsx', index_col=0)
     KNS_MkSiteCode = KNS_MkSiteCode[['KnsID', 'SiteId']]
     KNS_MkSiteCode.rename(columns={'KnsID': 'PersonID'}, inplace=True)
 
@@ -31,10 +31,10 @@ def person():
     missing_site_code = missing_site_code[['PersonID']]
     missing_site_code.drop_duplicates(inplace=True)
     missing_site_code.rename(columns={'PersonID': 'person_id'}, inplace=True)
-    missing_site_code.to_csv('../../data/reports/missing_site_code.csv', index=False)
+    missing_site_code.to_csv('../data/reports/missing_site_code.csv', index=False)
 
 
     # ----- save -----
     person.to_excel(
-        '../../data/model/dimensions/person.xlsx',
+        '../data/model/dimensions/person.xlsx',
         sheet_name='person', index=False)
